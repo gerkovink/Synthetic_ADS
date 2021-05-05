2d7233c0bdd6c1303b6a0e4b5881984209e28375
MMJHendriks <70342720+MMJHendriks@users.noreply.github.com>
2021-05-05 10:27:27 +0200

    Add files via upload

--- Code/AnalysisModel_SyntheticData.R
+++ Code/AnalysisModel_SyntheticData.R
@@ -1,11 +1,24 @@
+#Mirthe Hendriks 
+# 5-5-2021
+
 library(mice) #imputations
 library(readr)
 
 library(magrittr) # piping 
 library(dplyr) #data manipulation
 library(furrr) #parallel mapping
+library(corrplot)
+library(ggplot2)
+library(tidyr)
+library(tidyverse)
+library(purrr)
 
-setwd("C:\Users\mirth\OneDrive\Documenten\ADS\Thesis")
+library(broom.mixed) #pipe for mira? 
+
+#simulation  
+set.seed(123) #seed for reproducibility 
+
+setwd("~/ADS/Thesis")
 truedata <- read_csv("diabetes.csv", 
                  col_types = cols(Pregnancies = col_integer(), 
                                   Glucose = col_integer(), 
@@ -19,7 +32,7 @@ datad <- read_csv("diabetes.csv")
 
 #looking at the data 
 summary(truedata)
-cor(datad)
+dim(truedata)
 str(truedata)
 
 #complete model; logistic regression, all variables included
@@ -57,8 +70,20 @@ stat_properties<-function(x){
   return(unlist(c(mu_preg,mu_gluc,mu_BMI,var_preg,var_gluc,var_BMI,beta_preg,beta_gluc,beta_BMI,resvar,R2)))
 }
 
+#correlation variables 
+cor(datad)
+corrplot(cor(datad))
+
+distributions <- function(x){
+  ggplot(gather(x),aes(value))+
+    geom_histogram(bins=10)+
+    facet_wrap(~key, scales = 'free_x')
+}
+
+?ggplot
+
 #plot the distributions of the observed data 
-distributions<-function(x){ 
+distributions_model<-function(x){ 
   hist_preg <- hist(x$Pregnancies)
   hist_gluc <- hist(x$Glucose)
   hist_BMI <- hist(x$BMI)
@@ -66,11 +91,8 @@ distributions<-function(x){
 }
 
 stat_properties(truedata)
-distributions(truedata)
-
-
-#simulation  
-set.seed(123) #seed for reproducibility 
+distributions(datad)
+distributions_model(truedata)
 
 #parallel processing for an increased speed
 plan(multisession)
@@ -79,11 +101,10 @@ nsim=1000 #number of iterations, later change to 1000
 
 #specify method 
 meth <- make.method(truedata)
-#meth <- rep("pmm",ncol(truedata))  #OR this??
+meth <- rep("pmm",ncol(truedata))  
 names(meth) <- colnames(truedata)
-
-#??? Do we need to set Outcome to logistic regression
-#meth['Outcome'] <- "logreg"
+meth['Outcome'] <- "logreg"
+#meth['DiabetesPedigreeFunction'] <- "polr"
 
 #cart method, all variables are imputed by means of cart (classification and regression trees)
 cart <- rep("cart", ncol(truedata))
@@ -91,8 +112,6 @@ names(cart) <- colnames(truedata)
 
 #specify predictor matrix 
 pred <- make.predictorMatrix(truedata)
-pred
-# ??? passive imputation needed
 
 #default synthetic datasets
 syn_ds <- future_map(1:nsim, ~ { 
@@ -102,6 +121,8 @@ syn_ds <- future_map(1:nsim, ~ {
                     where=matrix(TRUE, nrow(truedata), ncol(truedata)),
                     print=FALSE)
 }, .options=future_options(seed=as.integer(123)), .progress=TRUE, .id = "syn")
+
+syn_ds[1]
 
 # in the output the imputation methods are empty "" for all variables, 
 # these empty strings imply that the variable is complete and therefore not imputed
@@ -120,20 +141,68 @@ syn_cart <- future_map(1:nsim, ~ {
 syn_cart[2]
 
 saveRDS(syn_cart, 'syn_cart_model.rds')
+syn_cart <- readRDS('syn_cart_model.rds')
 
-
-
-# some nodes for further steps
-simulation<-function(n){
+# pool function partially synthetic data
+pool3.syn <- function(mira) {
+  if(class(mira)[1] == "mira") { # if the input object is of class mira
+    fitlist <- mira %$% analyses # extract the analyses from the mira object
+  } 
+  else {
+    fitlist <- mira              # and otherwise, just take the input list
+  }
   
+  m <- length(fitlist)           # number of imputations
+  
+  pooled <- fitlist %>% 
+    map_dfr(broom::tidy) %>%
+    group_by(term) %>%
+    summarise(est     = mean(estimate),
+              bm      = sum((estimate - est)^2) / (m - 1),
+              ubar    = mean(std.error^2),
+              var     = ubar + bm/m, # new variance estimate
+              df      = (m - 1) * (1 + (ubar * m)/bm), # and new df estimate
+              lower   = est - qt(.975, df) * sqrt(var),
+              upper   = est + qt(.975, df) * sqrt(var), .groups = 'drop')
+  pooled
 }
 
-result <- replicate(nsim, simulation(n=nsim), simplify = FALSE)
+map_dfr(syn_ds, function(x) {
+  x %$%
+    glm(Outcome ~ Pregnancies + Glucose + BMI, family = binomial) %>% 
+    pool3.syn()
+})
+
+# this outputs a tibble of 4000 x 8, so not really a pooled output yet 
+
+map_dfr(syn_cart, function(x) {
+  x %$%
+    glm(Outcome ~ Pregnancies + Glucose + BMI, family = binomial) %>% 
+    pool3.syn()
+})
+
+syn_pooled <- map_dfr(syn_ds, function(x) {
+  x %$%
+    glm(Outcome ~ Pregnancies + Glucose + BMI, family = binomial) %>% 
+    pool3.syn()
+})
 
 
+pool3.syn(syn_ds)
+
+pool3.syn(syn_cart)
 
 
+## does not work yet 
+# get output
+syn_out <- syn_pooled %>%
+  map(function(x) summary(x, population.inference = TRUE)) %>%
+  map_dfr(function(x) {
+    coef(x) %>% 
+      as.data.frame %>%
+      rownames_to_column(var = "term")})
 
+syn_out
 
 
 
